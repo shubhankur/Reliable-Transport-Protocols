823059fbc1a547bc1e7f138bd56f84b37f9356bc
Shubhankar Kumar <shubhankur99@gmail.com>
2022-11-26 00:52:07 -0500

    first

--- src/abt.c
+++ src/abt.c
@@ -1,9 +1,16 @@
 #include "../include/simulator.h"
-#include "stdbool.h"
-#include "string.h"
-#include "stdio.h"
-#include "stdlib.h"
+#define TIMEOUT 20.0
+#define BUFFER_MSG 1000
+#define DEFAULT_ACK 111
+#define NULL 0
+#define   A    0
+#define   B    1
 
+/*All function declarations*/
+int calc_checksum(struct pkt *p);
+void append_msg(struct msg *m);
+struct node *pop_msg();
+/*end*/
 /* ******************************************************************
  ALTERNATING BIT AND GO-BACK-N NETWORK EMULATOR: VERSION 1.1  J.F.Kurose
 
@@ -18,93 +25,92 @@
 **********************************************************************/
 
 /********* STUDENTS WRITE THE NEXT SIX ROUTINES *********/
-//We create a buffer to store all the msgs
-struct buffer{
-  struct msg message;
-  struct buffer *next;
-};
 
-bool sender_state = true;
-struct pkt curr_packet;
-int seq_num_A = 0;
-int seq_num_B = 0;
-int get_checksum(struct pkt *packet);
-struct buffer *head = NULL;
-struct buffer *tail = NULL;
+
+struct pkt cur_packet;
+
+//sequence numbers
+int A_seqnum = 0;
+int B_seqnum = 0;
+
+//states
+int waiting_pkt=0;
+int waititng_ack=1;
+int sender_state=0;
+
+//buffer
+struct node {
+  struct msg message;
+  struct node *next;
+};
+struct node *list_head = NULL;
+struct node *list_end = NULL;
+
 
 /* called from layer 5, passed the data to be sent to other side */
 void A_output(message)
   struct msg message;
-{
-  //Creating new buffer using message
-  struct buffer *new = malloc(sizeof(struct buffer)); 
-  strncpy(new->message.data, message.data, 20);
-  printf(new->message.data);
+{ 
+  int checksum = 0;
+  struct node *n;
 
-  //Adding new buffer in the existing buffer
-  if(tail==NULL){
-    tail = new;
-    head = new;
+  append_msg(&message);//add msg to buffer
+  if(sender_state != waiting_pkt)
+  {
+    //do nothing
+    return;
   }
-  else{
-    tail->next = new;
-    tail = new;
-  }
-
-  //Retreive the first message in the buffer
-  struct buffer *curr_buffer = head;
-  head = head -> next;
-  if(head==NULL){
-    tail == NULL;
+  /* set current package to not finished */
+  sender_state = waititng_ack;
+  
+  n = pop_msg();
+  
+  if(n == NULL)
+  {
+    printf("No message need to process\n");
+    return 0;
   }
 
-  if(!sender_state) return;
+  for (int i=0; i<20; i++)
+  {
+    cur_packet.payload[i] = n->message.data[i];
+  }
+  free(n);
+  cur_packet.seqnum = A_seqnum;
+  cur_packet.acknum = DEFAULT_ACK;
+  checksum = calc_checksum(&cur_packet);
+  cur_packet.checksum = checksum;
+  tolayer3(A, cur_packet);
 
-  strncpy(curr_packet.payload, head->message.data, 20);
-  printf(curr_packet.payload);
-  curr_packet.acknum = 1;
-  curr_packet.seqnum = seq_num_A;
-  curr_packet.checksum = get_checksum(&curr_packet);
-  tolayer3(0, curr_packet);
-  sender_state = false;
-  starttimer(0, 10.0);
-}
-
-int get_checksum(struct pkt *pkt){
-    int result = 0;
-    if(pkt == NULL){
-        return result;
-    }
-    result = result + pkt->acknum;
-    result = result + pkt->seqnum;
-    int i = 0;
-    while(i<20){
-      result = result + (unsigned char)pkt->payload[i++];
-    }
-    return result;
+  starttimer(A, TIMEOUT);
 }
 
 /* called from layer 3, when a packet arrives for layer 4 */
 void A_input(packet)
   struct pkt packet;
 {
-  if(packet.checksum!=get_checksum(&packet)){
+  if(packet.checksum != calc_checksum(&packet))
+  {
     return;
   }
-  if(packet.acknum!=seq_num_A){
+
+   if(packet.acknum != A_seqnum)
+  {
     return;
   }
-  seq_num_A=1-seq_num_A;
-  sender_state = true;
-  //stoptimer(0);
+  A_seqnum = (A_seqnum + 1) % 2;
+  //stoptimer(A);
+  sender_state = waiting_pkt;
+
 }
 
 /* called when A's timer goes off */
 void A_timerinterrupt()
 {
-  if(sender_state==true){
-    tolayer3(0, curr_packet);
-    starttimer(0, 10.0);
+  if(sender_state == waititng_ack)
+  {
+    tolayer3(A, cur_packet);
+    starttimer(A, TIMEOUT);
   }
 }  
 
@@ -121,26 +127,28 @@ void A_init()
 void B_input(packet)
   struct pkt packet;
 {
-  if(packet.checksum != get_checksum(&packet))
+  if(packet.checksum != calc_checksum(&packet))
   {
     return;
   }
 
-  if(packet.seqnum != seq_num_B)
+  if(packet.seqnum != B_seqnum)
   {
-    return;
+   //Duplicated packet do nothing
   }
   /* normal package, deliver data to layer5 */
   else
   {
-    seq_num_B = 1- seq_num_B;
-    tolayer5(1, packet.payload);
+    B_seqnum = (B_seqnum + 1)%2;
+    tolayer5(B, packet.payload);
+    //Debug_Log(B, "Send packet to layer5", &packet, NULL);
   }
 
   /* send back ack */
   packet.acknum = packet.seqnum;
-  packet.checksum = get_checksum(&packet);
-  tolayer3(1, packet);
+  packet.checksum = calc_checksum(&packet);
+
+  tolayer3(B, packet);
 }
 
 /* the following routine will be called once (only) before any other */
@@ -148,4 +156,60 @@ void B_input(packet)
 void B_init()
 {
 
+}
+
+///////////////////////////////////////////////////
+//basic checksum function
+int calc_checksum(struct pkt *p)
+{
+  int i;
+  int checksum = 0;
+
+  if(p == NULL)
+  {
+    return checksum;
+  }
+  for (i=0; i<20; i++)
+  {
+    checksum += (unsigned char)p->payload[i];
+  }
+  checksum += p->seqnum;
+  checksum += p->acknum;
+  return checksum;
+}
+
+void append_msg(struct msg *m)
+{
+  int i;
+  struct node *n = malloc(sizeof(struct node));
+  if(n == NULL) {
+    printf("no enough memory\n");
+    return;
+  }
+  n->next = NULL;
+  for(i = 0; i < 20; ++i) {
+    n->message.data[i] = m->data[i];
+  }
+
+  if(list_end == NULL) 
+  {
+    list_head = n;
+    list_end = n;
+    return;
+  }
+  list_end->next = n;
+  list_end = n;
+}
+struct node *pop_msg()
+{
+  struct node *p;
+  if(list_head == NULL) {
+    return NULL;
+  }
+  p = list_head;
+  list_head = p->next;
+  if(list_head == NULL) {
+    list_end = NULL;
+  }
+  return p;
 }
